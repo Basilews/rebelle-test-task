e16c02024d6fcbb92d285b8e0a89d80e966aa5e4
Basilews <dethroll@gmail.com>
2018-09-18 14:49:12 +0300

    add fetch abort

--- src/js/script.js
+++ src/js/script.js
@@ -2,31 +2,58 @@ const form = document.getElementById('form');
 const searchInput = document.getElementById('id_search');
 const resultField = document.getElementById('result');
 const baseUrl = 'https://efounbqifq-dsn.algolia.net/1/indexes/Product_v2_en?';
-let timer;
+let timer, controller;
 
-
-const sendData = (value) => {
-  const xhr = new XMLHttpRequest();
+const fetchList = async(value) => {
+  controller = new AbortController();
+  const { signal } = controller;
+  console.log(signal);
+  const headers = new Headers({
+    'X-Algolia-API-Key': '2a92fd7cd4aca67298fbe1115fdef211',
+    'X-Algolia-Application-Id': 'EFOUNBQIFQ',
+  });
   const params = {
     query: value,
     hitsPerPage: 5,
   }
+  const options = {
+    method: 'GET',
+    headers,
+    mode: 'cors',
+    cache: 'default',
+    signal,
+  };
   const url = baseUrl + `query=${params.query}&hitsPerPage=${params.hitsPerPage}`;
 
-  xhr.open('GET', url, true);
-  xhr.setRequestHeader('X-Algolia-API-Key', '2a92fd7cd4aca67298fbe1115fdef211');
-  xhr.setRequestHeader('X-Algolia-Application-Id', 'EFOUNBQIFQ');
-  xhr.send();
+  resultField.innerHTML = 'Searching...';
 
-  if (xhr.status !== 200) {
-    resultField.innerHTML = xhr.status + ': ' + xhr.statusText;
-  } else {
-    resultField.innerHTML = JSON.parse(xhr.hits);
+  try {
+    const response = await fetch(url, options);
+    const data = await response.json();
+
+    resultField.innerHTML = '';
+
+    if (data.hits.length > 0) {
+      data.hits.forEach(item => (
+        resultField.innerHTML += `<div>${item.title}</div><br>`
+      ))
+    } else {
+      resultField.innerHTML = 'No results';
+    }
+  }
+  catch (err) {
+    if (err.name !== 'AbortError') {
+      resultField.textContent = "Oh no! Fetching failed.";
+    }
   }
 }
 
-searchInput.oninput = () => {
-  timer
-    ? clearTimeout(timer)
-    : timer = setTimeout(() => sendData(searchInput.value), 100);
-};
+searchInput.addEventListener('input', () => {
+  const { value } = searchInput;
+  if (!value) return;
+
+  if (timer) clearTimeout(timer);
+  timer = setTimeout(() => fetchList(value), 100)
+
+  if (controller) controller.abort();
+});
